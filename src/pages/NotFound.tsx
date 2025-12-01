ad0ccb2b3dafe605fdb8de2bda370553713b771b
maxctrdev~ <max.ctr.dev@gmail.com>
2025-11-30 23:12:37 -0500

    feat: enhance NotFoundPage with improved styling and user-friendly messaging

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,9 @@
 export const NotFoundPage = () => {
     return(
-        <div>
-            <span>404</span>
-            <p>Not Found Page</p>
-            <a href="/">return to main page</a>
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-6xl font-bold text-red-600">404</h1>
+            <p className="mt-4 text-lg text-gray-700">Oops! la página que buscas no existe.</p>
+            <a className="mt-6 px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600" href="/">Volver a la página principal</a>
         </div>
     )
 }
